0ec7a66477a99f749ba033384f8046a781abc4ef
TIMUR <1tkurbanov@gmail.com>
2019-05-06 15:09:09 -0400

    Print Name

--- exercise2.py
+++ exercise2.py
@@ -9,3 +9,10 @@ print ("What is the amount of the bill?")
 bill = input()
 tip = (int(bill) * 0.15)
 print (tip)
+
+#Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use print to print the result.
+print(str(99)+ " bottles of beer")
+
+#Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+
+"Result of multiplication 45628 and 7839 {}".format(45628 * 7839)
