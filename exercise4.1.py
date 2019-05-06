7ff46d2580867859085eb747e3207e7c643b2ec1
TIMUR <1tkurbanov@gmail.com>
2019-05-06 15:44:27 -0400

    4rth file

---
+++ exercise4.1.py
@@ -0,0 +1,6 @@
+print("Enter any number, please")
+number = input() #asking a user to enter a number
+if int(number) >= 100:
+    print("that's a big number")
+else:
+    print("why not dream a little bigger?")
