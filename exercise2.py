0de20a8d56c99c6535ce860b53f8af245fbc9839
TIMUR <1tkurbanov@gmail.com>
2019-05-06 13:21:06 -0400

    second ex

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+#How would you calculate a good tip for a 55 dollar meal? Use print to print the answer.
+#Steps: 1 - ask the user the bill value
+#Steps: 2 - user gives the value
+#Step: 3 - calculate the tip from the bill
+#Step: 4 - display the tip
+
+
+print ("What is the amount of the bill?")
+bill = input()
+tip = (int(bill) * 0.15)
+print (tip)
